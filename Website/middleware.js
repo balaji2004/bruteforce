a089fe6e1c5aa3b56a4edbfe0bc05f0d861bff00
balaji2004 <cvbalaji19672004@gmail.com>
2025-10-28 17:21:02 +0530

    Revert "Update middleware.js"
    This reverts commit 0d5f24dba3e76c3994759358427528a1ef6230e3.

--- Website/middleware.js
+++ Website/middleware.js
@@ -1,20 +1,14 @@
-import createMiddleware from 'next-intl/middleware';
-import { defaultLocale, locales } from './src/i18n/config';
+import { NextResponse } from 'next/server';
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales,
-  
-  // Used when no locale matches
-  defaultLocale,
-  
-  // Don't use locale prefixes in URLs
-  localePrefix: 'never'
-});
+export function middleware(request) {
+  const locale = request.cookies.get('NEXT_LOCALE')?.value || 'en';
+  const response = NextResponse.next();
+
+  response.headers.set('x-locale', locale);
+
+  return response;
+}
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
